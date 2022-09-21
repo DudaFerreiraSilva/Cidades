2f70b782451eb30bafd85a2d3655134d01b17f8d
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-09-21 13:25:37 -0300

    Exercício Completo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Mongaguá");
+Thread.Sleep(1000);
+
+Console.WriteLine("Santos");
+Thread.Sleep(1000);
+
+Console.WriteLine("Itanháem");
+Thread.Sleep(1000);
+
